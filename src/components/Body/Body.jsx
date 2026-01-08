8614b6ccb0624194d17d8c920c02801ace05a13f
Nick <melancholic8078@gmail.com>
2026-01-09 01:52:14 +0200

    added task to localstorage

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import css from '../Body/Body.module.css'
 import Task from '../Task/Task'
 import CreateTaskBar from '../CreateTaskBar/CreateTaskBar'
 
 const Body = () => {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState(()=>{
+    const savedTasks = localStorage.getItem('task')
+    if(savedTasks !=null){
+        return JSON.parse(savedTasks)
+    }
+    return []
+  })
 
   const taskAdd = (text)=>{
     setTasks(prev =>[...prev,{
@@ -14,6 +20,9 @@ const Body = () => {
     }])
   }
 
+  useEffect(()=>{
+    localStorage.setItem('task', JSON.stringify(tasks),[tasks])    
+  })
   return (
     <div className={css.body_section}>
         <ul className={css.body_ul}>
